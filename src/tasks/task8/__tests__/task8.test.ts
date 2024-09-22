735d56b5a9a82f1fd96303906c42d52cdd089b74
NotNikita <nikitabsuir@gmail.com>
2024-09-22 23:29:45 +0200

    task8 end

--- src/tasks/task8/__tests__/task8.test.ts
+++ src/tasks/task8/__tests__/task8.test.ts
@@ -1,12 +1,21 @@
-import {getRoute} from '../task8';
+import {getRoute, getRouteFaster} from '../task8';
 
 describe('task8', () => {
   it('should get correct route 1', () => {
+    const start = performance.now();
     const result = getRoute();
-    expect(result).toEqual([
-      {from: 'Dubai', to: 'Astana'},
-      {from: 'Astana', to: 'Bali'},
-      {from: 'Bali', to: 'Dublin'},
-    ]);
+    const end = performance.now();
+    const duration = end - start;
+    console.log(`getRoute took ${duration}ms`); // 0.215s
+    expect(result).toEqual(['Dubai', 'Astana', 'Bali', 'Dublin']);
+  });
+
+  it('should get correct route faster', () => {
+    const start = performance.now();
+    const result = getRouteFaster();
+    const end = performance.now();
+    const duration = end - start;
+    console.log(`getRouteFaster took ${duration}ms`); // 0.094ms
+    expect(result).toEqual(['Dubai', 'Astana', 'Bali', 'Dublin']);
   });
 });
--- src/tasks/task8/task8.ts
+++ src/tasks/task8/task8.ts
@@ -26,7 +26,7 @@ export const mockRoutes: Ticket[] = [
   {from: 'Bali', to: 'Dublin'},
 ];
 
-export const getRoute = (tickets = mockRoutes) => {
+export function getRoute(tickets = mockRoutes) {
   // code here
   // connections map
   let connections: {[key: string]: string[]} = {};
@@ -38,14 +38,57 @@ export const getRoute = (tickets = mockRoutes) => {
     }
   });
   const fromArray = Object.keys(connections);
-  const startingAirport = Object.values(connections)
+  const endingAirport = Object.values(connections)
     .map(toAirports => toAirports.find(airport => !fromArray.includes(airport)))
     .filter(v => v);
-  console.log(connections);
-  console.log('fromArray', fromArray);
-  console.log('startingAirport', startingAirport);
 
-  if (!startingAirport.length) {
+  if (!endingAirport || !endingAirport.length) {
     return "Error: We couldn't find staring airport";
   }
-};
+  let resultRoute = [endingAirport[0]];
+  let currAirport: string | undefined = endingAirport[0];
+  // we are starting from the end (reverse BFS?)
+  while (currAirport && typeof currAirport === 'string') {
+    const prevAirportKV = Object.entries(connections).find(([key, value]) => {
+      return value.includes(currAirport as string) ? key : false;
+    });
+    if (prevAirportKV) {
+      const [prevAirportName, _] = prevAirportKV;
+
+      resultRoute.push(prevAirportName);
+      currAirport = prevAirportName;
+    } else {
+      // We reached the end
+      currAirport = undefined;
+    }
+  }
+
+  return resultRoute.reverse();
+}
+
+export function getRouteFaster(tickets = mockRoutes) {
+  // const destinationAirports = tickets.map(t => t.to);
+  const destinationAirports = new Set(tickets.map(t => t.to));
+  // the start airport is the one, that was never on 'to' key. Opposit goes to end airport
+  const startAirport = tickets.find(ticket => !destinationAirports.has(ticket.from))?.from;
+
+  // Create a map for quick lookup of the next airport
+  const airportMap = new Map(tickets.map(ticket => [ticket.from, ticket.to]));
+
+  let result = [startAirport];
+  let currentAirport: string | undefined = startAirport;
+
+  while (currentAirport) {
+    const someNextAirport = airportMap.get(currentAirport);
+
+    if (someNextAirport) {
+      currentAirport = someNextAirport;
+      result.push(someNextAirport);
+    } else {
+      // stop cycle;
+      currentAirport = someNextAirport;
+    }
+  }
+
+  return result;
+}
